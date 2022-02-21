15e95c925ae985ed18e7328a1217b02b5652a52c
MamaCheli <90408246+MamaCheli@users.noreply.github.com>
2022-02-21 16:56:09 +0300

    Обход директории
    Такой же обход, как в задаче 7 с проверкой на то, чтобы не заходили в backup, + копирование в backup

--- run.sh
+++ run.sh
@@ -5,3 +5,23 @@ cd $1
 extension=$2
 backup=$3
 mkdir $backup
+
+bypass() {
+  folder=$(ls)
+  for file in $folder
+  do
+    if [[ "$file" != $backup ]]
+    then
+      if [ -d $file ]
+      then
+        cd $file
+        bypass
+        cd ..
+      fi
+      if [[ $file == *.$extension ]]
+      then
+        cp $file $path/$backup/$file
+      fi
+    fi
+  done
+}
